ee686e9fd66ed16f9831288e93684f50477516e5
Luciano Lima <lucianolima122123@gmail.com>
2021-12-05 17:46:28 -0300

    Initial commit

---
+++ src/main/java/com/example/noteedu/NoteEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.noteedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noteedu/NoteEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noteedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
