847fd0bace1930549b8157c54049b8e18e4fcde6
Luciano Lima <lucianolima122123@gmail.com>
2021-12-15 08:51:58 -0300

    user controller, model

---
+++ src/main/java/com/example/noteedu/user/UserService.java
@@ -0,0 +1,13 @@
+package com.example.noteedu.user;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+
+public class UserService {
+
+    public List<User> getUsers() {
+        return null;
+    }
+}
