2b8c594d8bfd3e1aef13d0a5a245537c4f8ad2d9
miriamcomex123 <miriamcomex123@gmail.com>
2022-02-02 20:23:21 -0300

    Adicionando gitignore e AddReminder front

---
+++ frontend/src/containers/AddReminder.js
@@ -0,0 +1,9 @@
+import React, {useHistory} from 'react';
+
+export const AddReminder = ({setUsername}) => {
+  const [error, setError] = useState();
+  function handleSubmit(event){
+      event.preventDefault();
+  }
+    return <div></div>;
+};
